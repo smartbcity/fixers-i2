8f196f23513e8f5d60f6c52462cc8df3e71c0cfa
Luc Heydel <luc@smartb.city>
2021-02-17 17:04:29 +0100

    initialize gradle project

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,12 @@
+object PluginVersions {
+    const val spring = "2.4.1"
+    const val kotlin = "1.4.30"
+    const val jib = "2.7.0"
+    const val springPom = "1.0.10.RELEASE"
+}
+
+object Versions {
+    const val junit = "5.7.0"
+    const val assertj = "3.15.0"
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        jcenter()
+    }
+}
+
+rootProject.name = "i2"
+
+enableFeaturePreview("GRADLE_METADATA")
+
+include(
+)
