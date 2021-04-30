dbe8a2ff269820c047659faf40f580b733414600
Luc Heydel <luc@smartb.city>
2021-04-30 18:12:31 +0200

    keycloak role get page

--- i2-s2/role/role-f2-query/src/main/kotlin/i2/s2/role/f2/RoleModelExtension.kt
+++ i2-s2/role/role-f2-query/src/main/kotlin/i2/s2/role/f2/RoleModelExtension.kt
@@ -3,6 +3,8 @@ package i2.s2.role.f2
 import i2.s2.role.domain.RoleModel
 import org.keycloak.representations.idm.RoleRepresentation
 
+fun List<RoleRepresentation>.asModels() = map(RoleRepresentation::asModel)
+
 fun RoleRepresentation.asModel() = RoleModel(
     id = id,
     description = description,
---
+++ i2-test/test-it/src/test/kotlin/i2/test/it/role/RoleGetPageQueryFunctionImplTest.kt
@@ -0,0 +1,41 @@
+package i2.test.it.role
+
+import f2.dsl.cqrs.base.PageRequestBase
+import f2.function.spring.invokeSingle
+import i2.s2.role.domain.features.query.RoleGetPageQuery
+import i2.s2.role.f2.RoleGetPageQueryFunctionImpl
+import i2.test.bdd.given.GivenKC
+import i2.test.bdd.given.auth
+import i2.test.bdd.given.role
+import i2.test.bdd.testcontainers.I2KeycloakTest
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class RoleGetPageQueryFunctionImplTest: I2KeycloakTest() {
+
+	val client = GivenKC().auth().withMasterRealmClient()
+
+	@Test
+	fun `should get page of role`(): Unit = runBlocking {
+		val existingRoles = client.roles().list()
+		val newRoles = (0..6).map { _ ->
+			GivenKC(client).role().withRole(UUID.randomUUID().toString())
+		}
+
+		val cmd = RoleGetPageQuery(
+			realmId = client.auth.realmId,
+			auth = client.auth,
+			page = PageRequestBase(
+				page = 0,
+				size = 5
+			)
+		)
+		val result = RoleGetPageQueryFunctionImpl().roleGetPageQueryFunctionImpl().invokeSingle(cmd)
+
+		Assertions.assertThat(result.page.list).hasSize(cmd.page.size!!)
+		Assertions.assertThat(result.page.total).isEqualTo(existingRoles.size + newRoles.size.toLong())
+	}
+
+}
