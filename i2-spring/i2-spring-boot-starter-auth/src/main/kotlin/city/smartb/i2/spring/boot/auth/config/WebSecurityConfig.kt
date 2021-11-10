c9a61ccb4db3e19158d26bdb686604b2c32892a4
Luc Heydel <luc@smartb.city>
2021-11-10 09:45:27 +0100

    make web security config customizable

--- i2-spring/i2-spring-boot-starter-auth/src/main/kotlin/city/smartb/i2/spring/boot/auth/config/WebSecurityConfig.kt
+++ i2-spring/i2-spring-boot-starter-auth/src/main/kotlin/city/smartb/i2/spring/boot/auth/config/WebSecurityConfig.kt
@@ -3,18 +3,17 @@ package city.smartb.i2.spring.boot.auth.config
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.security.authorization.AuthorizationDecision
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authorization.AuthorizationContext
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 import reactor.core.publisher.Mono
-import java.util.HashMap
 
-@Configuration
-class WebSecurityConfig {
+interface WebSecurityConfig {
 
     companion object {
         const val SPRING_SECURITY_FILTER_CHAIN = "springSecurityFilterChain"
@@ -29,20 +28,26 @@ class WebSecurityConfig {
     fun dummyAuthenticationProvider(http: ServerHttpSecurity): SecurityWebFilterChain {
         http.authorizeExchange().anyExchange().permitAll()
         http.csrf().disable()
+        http.corsConfig()
         return http.build()
     }
 
     @Bean(SPRING_SECURITY_FILTER_CHAIN)
     @ConditionalOnExpression(AUTHENTICATION_REQUIRED_EXPRESSION)
     fun oauthAuthenticationProvider(http: ServerHttpSecurity): SecurityWebFilterChain {
-        http.authorizeExchange()
-                .anyExchange()
-                .access(::authenticate)
-                .and()
-                .oauth2ResourceServer()
-                .jwt()
+        addAuthenticationRules(http)
         http.csrf().disable()
+        http.corsConfig()
         return http.build()
+    }
+
+    fun addAuthenticationRules(http: ServerHttpSecurity) {
+        http.authorizeExchange()
+            .anyExchange()
+            .access(::authenticate)
+            .and()
+            .oauth2ResourceServer()
+            .jwt()
     }
 
     private fun authenticate(authentication: Mono<Authentication>, context: AuthorizationContext): Mono<AuthorizationDecision> {
@@ -54,5 +59,17 @@ class WebSecurityConfig {
             val filters = authFilter()
             filters.isEmpty() || filters.all { (key, value) -> auth.token.claims[key] == value }
         }.map(::AuthorizationDecision)
+    }
+
+    private fun ServerHttpSecurity.corsConfig() {
+        val config = CorsConfiguration()
+        config.allowedOrigins = listOf("*")
+        config.allowCredentials = false
+        config.addAllowedMethod("*")
+        config.addAllowedHeader("*")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+        cors().configurationSource(source)
     }
 }
