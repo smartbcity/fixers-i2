d59aa00162879a91b8df644f19c9c5725e6eac00
teddy <teddy@smartb.city>
2022-03-01 14:52:16 +0100

    add GetAllOrganizationRef endpoint

---
+++ i2-f2/organization/organization-f2-query/src/main/kotlin/i2/f2/organization/app/OrganizationRefGetAllQueryFunctionImpl.kt
@@ -0,0 +1,32 @@
+package i2.f2.organization.app
+
+import f2.dsl.fnc.f2Function
+import f2.dsl.fnc.invoke
+import i2.f2.config.I2KeycloakConfig
+import i2.f2.organization.app.model.toOrganizationRef
+import i2.f2.organization.domain.features.query.*
+import i2.keycloak.f2.group.domain.features.query.GroupGetAllQuery
+import i2.keycloak.f2.group.domain.features.query.GroupGetAllQueryFunction
+import i2.keycloak.f2.group.domain.model.GroupModel
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OrganizationRefGetAllQueryFunctionImpl(
+	private val groupGetAllQueryFunction: GroupGetAllQueryFunction,
+	private val i2KeycloakConfig: I2KeycloakConfig
+) {
+
+	@Bean
+	fun organizationRefGetAllQueryFunction(): OrganizationRefGetAllQueryFunction = f2Function { cmd ->
+		groupGetAllQueryFunction.invoke(cmd.toGroupGetAllQuery())
+			.groups
+			.map(GroupModel::toOrganizationRef)
+			.let(::OrganizationRefGetAllQueryResult)
+	}
+
+	private fun OrganizationRefGetAllQuery.toGroupGetAllQuery() = GroupGetAllQuery(
+		realmId = i2KeycloakConfig.realm,
+		auth = i2KeycloakConfig.authRealm()
+	)
+}
--- i2-f2/organization/organization-f2-query/src/main/kotlin/i2/f2/organization/app/model/OrganizationExtension.kt
+++ i2-f2/organization/organization-f2-query/src/main/kotlin/i2/f2/organization/app/model/OrganizationExtension.kt
@@ -4,6 +4,7 @@ import i2.commons.model.AddressBase
 import i2.commons.utils.parseJsonTo
 import i2.f2.organization.domain.model.Organization
 import i2.f2.organization.domain.model.OrganizationBase
+import i2.f2.organization.domain.model.OrganizationRefBase
 import i2.keycloak.f2.group.domain.model.GroupModel
 
 fun GroupModel.toOrganization() = OrganizationBase(
@@ -13,4 +14,9 @@ fun GroupModel.toOrganization() = OrganizationBase(
     address = attributes[Organization::address.name]?.first()?.parseJsonTo(AddressBase::class.java).orEmpty(),
     description = attributes[Organization::description.name]?.firstOrNull(),
     website = attributes[Organization::website.name]?.firstOrNull()
+)
+
+fun GroupModel.toOrganizationRef() = OrganizationRefBase(
+    id = id,
+    name = name
 )
