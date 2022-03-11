0cee91e7917bf212b26118dac69847f808614d43
Luc Heydel <luc@smartb.city>
2022-03-11 09:53:36 +0100

    enable the use of RolesAllowed and PermitAll annotations for endpoints

--- i2-spring/i2-spring-boot-starter-auth/src/main/kotlin/city/smartb/i2/spring/boot/auth/config/WebSecurityConfig.kt
+++ i2-spring/i2-spring-boot-starter-auth/src/main/kotlin/city/smartb/i2/spring/boot/auth/config/WebSecurityConfig.kt
@@ -2,22 +2,33 @@ package city.smartb.i2.spring.boot.auth.config
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.security.authorization.AuthorizationDecision
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authorization.AuthorizationContext
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import javax.annotation.security.PermitAll
+import javax.annotation.security.RolesAllowed
 
 interface WebSecurityConfig {
 
     companion object {
         const val SPRING_SECURITY_FILTER_CHAIN = "springSecurityFilterChain"
     }
+
+    val contextPath: String
+    val applicationContext: ApplicationContext
 
     @Bean
     @ConfigurationProperties(prefix = "i2.filter")
@@ -42,12 +53,10 @@ interface WebSecurityConfig {
     }
 
     fun addAuthenticationRules(http: ServerHttpSecurity) {
-        http.authorizeExchange()
-            .anyExchange()
-            .access(::authenticate)
-            .and()
-            .oauth2ResourceServer()
-            .jwt()
+        addRolesAllowedRules(http)
+        addPermitAllRules(http)
+        addMandatoryAuthRules(http)
+        addJwtParsingRules(http)
     }
 
     private fun authenticate(authentication: Mono<Authentication>, context: AuthorizationContext): Mono<AuthorizationDecision> {
@@ -71,5 +80,53 @@ interface WebSecurityConfig {
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", config)
         cors().configurationSource(source)
+    }
+
+    fun addRolesAllowedRules(http: ServerHttpSecurity) {
+        applicationContext.getBeanNamesForAnnotation(RolesAllowed::class.java)
+            .associateWith { bean ->
+                applicationContext.findAnnotationOnBean(bean, RolesAllowed::class.java)!!.value
+            }
+            .forEach { (name, roles) ->
+                http.authorizeExchange()
+                    .pathMatchers("$contextPath/$name")
+                    .hasAnyRole(*roles)
+            }
+    }
+
+    fun addPermitAllRules(http: ServerHttpSecurity) {
+        val permitAllBeans = applicationContext.getBeanNamesForAnnotation(PermitAll::class.java)
+            .map { bean -> "$contextPath/$bean" }
+            .toTypedArray()
+
+        if (permitAllBeans.isNotEmpty()) {
+            http.authorizeExchange()
+                .pathMatchers(*permitAllBeans)
+                .permitAll()
+        }
+    }
+
+    fun addMandatoryAuthRules(http: ServerHttpSecurity) {
+        http.authorizeExchange()
+            .anyExchange()
+            .access(::authenticate)
+    }
+
+    fun addJwtParsingRules(http: ServerHttpSecurity) {
+        http.oauth2ResourceServer()
+            .jwt { jwtSpec -> jwtSpec.jwtAuthenticationConverter(jwtAuthenticationConverter()) }
+    }
+
+    private fun jwtAuthenticationConverter(): ReactiveJwtAuthenticationConverter {
+        return ReactiveJwtAuthenticationConverter().apply {
+            setJwtGrantedAuthoritiesConverter(::jwtAuthoritiesConverter)
+        }
+    }
+
+    private fun jwtAuthoritiesConverter(jwt: Jwt): Flux<GrantedAuthority> {
+        val realmAccess = jwt.claims["realm_access"]!! as Map<String, List<String>>
+        return realmAccess["roles"]!!.map { role ->
+            SimpleGrantedAuthority("ROLE_$role")
+        }.let { Flux.fromIterable(it) }
     }
 }
