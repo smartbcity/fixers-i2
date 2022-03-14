108468c937f43d2c9cf7af9fe2bb239c9903354c
Luc Heydel <luc@smartb.city>
2022-03-14 09:26:47 +0100

    fix sample app

--- sample/spring-boot-app/src/main/kotlin/i2/app/config/I2WebSecurityConfig.kt
+++ sample/spring-boot-app/src/main/kotlin/i2/app/config/I2WebSecurityConfig.kt
@@ -1,7 +1,12 @@
 package i2.app.config
 
 import city.smartb.i2.spring.boot.auth.config.WebSecurityConfig
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class I2WebSecurityConfig: WebSecurityConfig
+class I2WebSecurityConfig(
+    override val applicationContext: ApplicationContext
+): WebSecurityConfig {
+    override val contextPath: String = ""
+}
