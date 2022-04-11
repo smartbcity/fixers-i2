a832d7bb821b583a4e0eb3a0772cc1e97578b8d1
Adrien Poupard <adrien@smartb.city>
2022-04-11 16:03:54 +0200

    Fix wording

--- i2-f2/i2-user/i2-user-domain/src/commonMain/kotlin/i2.f2.user.domain/features/command/UserResetPasswordFunction.kt
+++ i2-f2/i2-user/i2-user-domain/src/commonMain/kotlin/i2.f2.user.domain/features/command/UserResetPasswordFunction.kt
@@ -5,13 +5,13 @@ import f2.dsl.cqrs.Event
 import f2.dsl.fnc.F2Function
 import i2.f2.user.domain.model.UserId
 
-typealias UserResetPasswordFunction = F2Function<UserResetPasswordCommand, UserPasswordResettedResult>
+typealias UserResetPasswordFunction = F2Function<UserResetPasswordCommand, UserPasswordResetResult>
 
 data class UserResetPasswordCommand(
     val id: UserId,
     val password: String
 ): Command
 
-data class UserPasswordResettedResult(
+data class UserPasswordResetResult(
     val id: UserId
 ): Event
