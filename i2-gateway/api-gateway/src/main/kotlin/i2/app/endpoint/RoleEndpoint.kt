f15b9ba3d923befc70e015bb7d07eadcbad57d24
teddy <teddy@smartb.city>
2022-04-21 12:20:48 +0200

    add security to endpoints

--- i2-gateway/api-gateway/src/main/kotlin/i2/app/endpoint/RoleEndpoint.kt
+++ i2-gateway/api-gateway/src/main/kotlin/i2/app/endpoint/RoleEndpoint.kt
@@ -1,8 +1,10 @@
 package i2.app.endpoint
 
+import i2.app.auth.SUPER_ADMIN_ROLE
 import i2.f2.role.domain.features.command.RoleAddCompositesFunction
 import i2.f2.role.domain.features.command.RoleCreateFunction
 import i2.f2.role.domain.features.command.RoleUpdateFunction
+import javax.annotation.security.RolesAllowed
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -14,11 +16,14 @@ class RoleEndpoint(
 ) {
 
     @Bean
+    @RolesAllowed(SUPER_ADMIN_ROLE)
     fun roleAddComposites() = roleAddCompositesFunction
 
     @Bean
+    @RolesAllowed(SUPER_ADMIN_ROLE)
     fun roleCreate() = roleCreateFunction
 
     @Bean
+    @RolesAllowed(SUPER_ADMIN_ROLE)
     fun roleUpdate() = roleUpdateFunction
 }
--- i2-gateway/api-gateway/src/main/kotlin/i2/app/endpoint/UserEndpoint.kt
+++ i2-gateway/api-gateway/src/main/kotlin/i2/app/endpoint/UserEndpoint.kt
@@ -1,10 +1,16 @@
 package i2.app.endpoint
 
+import f2.dsl.fnc.f2Function
+import f2.dsl.fnc.invoke
+import i2.app.auth.PermissionEvaluator
+import i2.app.auth.SUPER_ADMIN_ROLE
 import i2.f2.user.domain.features.command.UserCreateFunction
 import i2.f2.user.domain.features.command.UserResetPasswordFunction
 import i2.f2.user.domain.features.command.UserUpdateFunction
 import i2.f2.user.domain.features.query.UserGetAllQueryFunction
+import i2.f2.user.domain.features.query.UserGetByIdQuery
 import i2.f2.user.domain.features.query.UserGetByIdQueryFunction
+import javax.annotation.security.RolesAllowed
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -14,21 +20,46 @@ class UserEndpoint(
     private val userResetPasswordFunction: UserResetPasswordFunction,
     private val userUpdateFunction: UserUpdateFunction,
     private val userGetByIdQueryFunction: UserGetByIdQueryFunction,
-    private val userGetAllQueryFunction: UserGetAllQueryFunction
+    private val userGetAllQueryFunction: UserGetAllQueryFunction,
+    private val permissionEvaluator: PermissionEvaluator
 ) {
 
     @Bean
-    fun createUser() = userCreateFunction
+    @RolesAllowed(SUPER_ADMIN_ROLE, "write_user")
+    fun createUser(): UserCreateFunction = f2Function { cmd ->
+        if (permissionEvaluator.isSuperAdmin() || permissionEvaluator.checkOrganizationId(cmd.memberOf)) {
+            userCreateFunction.invoke(cmd)
+        } else {
+            throw IllegalAccessException("Access denied.")
+        }
+    }
 
     @Bean
-    fun updateUser() = userUpdateFunction
+    @RolesAllowed(SUPER_ADMIN_ROLE, "write_user")
+    fun updateUser(): UserUpdateFunction = f2Function { cmd ->
+        if (permissionEvaluator.isSuperAdmin() || permissionEvaluator.checkOrganizationId(cmd.memberOf)) {
+            userUpdateFunction.invoke(cmd)
+        } else {
+            throw IllegalAccessException("Access denied.")
+        }
+    }
 
     @Bean
-    fun resetUserPassword() = userResetPasswordFunction
+    @RolesAllowed(SUPER_ADMIN_ROLE, "write_user")
+    fun resetUserPassword(): UserResetPasswordFunction = f2Function { cmd ->
+        val user = userGetByIdQueryFunction.invoke(UserGetByIdQuery(cmd.id)).user
+        if (permissionEvaluator.isSuperAdmin() || permissionEvaluator.checkOrganizationId(user?.memberOf?.id)) {
+            userResetPasswordFunction.invoke(cmd)
+        } else {
+            throw IllegalAccessException("Access denied.")
+        }
+    }
 
     @Bean
+    @RolesAllowed(SUPER_ADMIN_ROLE, "read_user")
     fun getUser() = userGetByIdQueryFunction
 
     @Bean
+    @RolesAllowed(SUPER_ADMIN_ROLE, "read_user")
     fun getAllUsers() = userGetAllQueryFunction
 }
