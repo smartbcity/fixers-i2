cf5a28e312e3519e90ff3399859bf29c4b4b82cd
teddy <teddy@smartb.city>
2022-05-11 14:08:03 +0200

    add composites roles to org and orgRef

--- i2-keycloak/keycloak-f2/group/group-f2-query/src/main/kotlin/i2/keycloak/f2/group/app/model/GroupRepresentationExtension.kt
+++ i2-keycloak/keycloak-f2/group/group-f2-query/src/main/kotlin/i2/keycloak/f2/group/app/model/GroupRepresentationExtension.kt
@@ -3,9 +3,9 @@ package i2.keycloak.f2.group.app.model
 import i2.keycloak.f2.group.domain.model.GroupModel
 import org.keycloak.representations.idm.GroupRepresentation
 
-fun GroupRepresentation.asModel() = GroupModel(
+fun GroupRepresentation.asModel(getComposites: (roleName: String) -> List<String>) = GroupModel(
     id = id,
     name = name,
     attributes = attributes ?: emptyMap(),
-    roles = realmRoles ?: emptyList()
+    roles = realmRoles.orEmpty().flatMap{ getComposites(it) + it }.distinct()
 )
