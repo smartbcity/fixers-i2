1177ed57d471e696061a2d4c58047da140ceb550
teddy <teddy@smartb.city>
2022-05-13 15:29:39 +0200

    edit storybook config

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -1,5 +1,5 @@
-import { ThemeContextProvider } from "@smartb/g2-themes";
-import { StorybookCanvas } from "@smartb/g2-storybook-documentation";
+import {ThemeContextProvider} from "@smartb/g2-themes";
+import {StorybookCanvas} from "@smartb/g2-storybook-documentation";
 
 import "./default.css";
 
