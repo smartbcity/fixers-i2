24c937694ec3f5e48a265e1b5cb5cf988a7714d4
Luc Heydel <luc@smartb.city>
2022-07-18 14:53:29 +0200

    fix(user): filter default realm role from assignedRoles

--- i2-keycloak/keycloak-f2/user/user-f2-query/src/main/kotlin/i2/keycloak/f2/user/app/service/UserFinderService.kt
+++ i2-keycloak/keycloak-f2/user/user-f2-query/src/main/kotlin/i2/keycloak/f2/user/app/service/UserFinderService.kt
@@ -5,6 +5,7 @@ import i2.keycloak.f2.user.domain.features.query.UserGetRolesFunction
 import i2.keycloak.f2.user.domain.features.query.UserGetRolesQuery
 import i2.keycloak.f2.user.domain.model.UserId
 import i2.keycloak.f2.user.domain.model.UserRoles
+import i2.keycloak.f2.user.domain.model.defaultRealmRole
 import i2.keycloak.master.domain.AuthRealm
 import i2.keycloak.master.domain.RealmId
 import org.springframework.stereotype.Service
@@ -20,8 +21,10 @@ class UserFinderService(
             auth = authRealm
         ).invokeWith(userGetRolesQueryFunction)
 
+        val defaultRealmRole = defaultRealmRole(realmId)
+
         return UserRoles(
-            assignedRoles = query.roles.assignedRoles,
+            assignedRoles = query.roles.assignedRoles.filter { it != defaultRealmRole },
             effectiveRoles = query.roles.effectiveRoles
         )
     }
--- i2-keycloak/keycloak-f2/user/user-f2-roles-revoke/src/main/kotlin/i2/keycloak/f2/user/app/UserRolesRevokeFunctionImpl.kt
+++ i2-keycloak/keycloak-f2/user/user-f2-roles-revoke/src/main/kotlin/i2/keycloak/f2/user/app/UserRolesRevokeFunctionImpl.kt
@@ -1,11 +1,13 @@
 package i2.keycloak.f2.user.app
 
-import f2.dsl.fnc.f2Function
+import i2.keycloak.f2.commons.app.keycloakF2Function
 import i2.keycloak.f2.user.domain.features.command.UserRolesRevokeFunction
 import i2.keycloak.f2.user.domain.features.command.UserRolesRevokedEvent
 import i2.keycloak.f2.user.domain.model.UserId
+import i2.keycloak.f2.user.domain.model.defaultRealmRole
+import i2.keycloak.master.domain.RealmId
 import i2.keycloak.realm.client.config.AuthRealmClient
-import i2.keycloak.realm.client.config.AuthRealmClientBuilder
+import i2.keycloak.realm.client.config.realmsResource
 import org.keycloak.admin.client.resource.RoleScopeResource
 import org.keycloak.representations.idm.RoleRepresentation
 import org.springframework.context.annotation.Bean
@@ -15,24 +17,27 @@ import org.springframework.context.annotation.Configuration
 class UserRolesRevokeFunctionImpl {
 
 	@Bean
-	fun userRolesRevokeFunction(): UserRolesRevokeFunction = f2Function { cmd ->
-		val realmClient = AuthRealmClientBuilder().build(cmd.auth)
-		realmClient.removeUserRole(cmd.id, cmd.roles)
+	fun userRolesRevokeFunction(): UserRolesRevokeFunction = keycloakF2Function { cmd, client ->
+		val rolesToRevoke = defaultRealmRole(cmd.realmId).let { defaultRole ->
+			cmd.roles.filter { it != defaultRole }
+		}
+
+		client.removeUserRole(cmd.id, rolesToRevoke, cmd.realmId)
 		UserRolesRevokedEvent(cmd.id)
 	}
 
-	fun AuthRealmClient.removeUserRole(userId: UserId, roles: List<String>) {
+	fun AuthRealmClient.removeUserRole(userId: UserId, roles: List<String>, realmId: RealmId) {
 		val roleRepresentations = roles.map { role ->
-			getRoleRepresentation(role)
+			getRoleRepresentation(role, realmId)
 		}
-		getUserRealmRolesResource(userId).remove(roleRepresentations)
+		getUserRealmRolesResource(userId, realmId).remove(roleRepresentations)
 	}
 
-	fun AuthRealmClient.getRoleRepresentation(role: String): RoleRepresentation {
-		return realm.roles().get(role).toRepresentation()
+	fun AuthRealmClient.getRoleRepresentation(role: String, realmId: RealmId): RoleRepresentation {
+		return realmsResource(realmId).roles().get(role).toRepresentation()
 	}
 
-	private fun AuthRealmClient.getUserRealmRolesResource(userId: String): RoleScopeResource {
-		return getUserResource(userId).roles().realmLevel()
+	private fun AuthRealmClient.getUserRealmRolesResource(userId: String, realmId: RealmId): RoleScopeResource {
+		return getUserResource(userId, realmId).roles().realmLevel()
 	}
 }
