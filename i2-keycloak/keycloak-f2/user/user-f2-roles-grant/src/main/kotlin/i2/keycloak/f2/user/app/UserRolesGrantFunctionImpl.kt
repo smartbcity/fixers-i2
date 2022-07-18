61ef8e076167689e2fa01861375eac813690aa3e
Luc Heydel <luc@smartb.city>
2022-07-18 14:54:41 +0200

    cleanup

--- i2-keycloak/keycloak-f2/user/user-f2-roles-grant/src/main/kotlin/i2/keycloak/f2/user/app/UserRolesGrantFunctionImpl.kt
+++ i2-keycloak/keycloak-f2/user/user-f2-roles-grant/src/main/kotlin/i2/keycloak/f2/user/app/UserRolesGrantFunctionImpl.kt
@@ -1,13 +1,12 @@
 package i2.keycloak.f2.user.app
 
-import f2.dsl.fnc.f2Function
+import i2.keycloak.f2.commons.app.keycloakF2Function
 import i2.keycloak.f2.user.domain.features.command.UserRolesGrantCommand
 import i2.keycloak.f2.user.domain.features.command.UserRolesGrantFunction
 import i2.keycloak.f2.user.domain.features.command.UserRolesGrantedEvent
 import i2.keycloak.f2.user.domain.model.UserId
 import i2.keycloak.master.domain.RealmId
 import i2.keycloak.realm.client.config.AuthRealmClient
-import i2.keycloak.realm.client.config.AuthRealmClientBuilder
 import org.keycloak.admin.client.resource.RoleScopeResource
 import org.keycloak.representations.idm.RoleRepresentation
 import org.springframework.context.annotation.Bean
@@ -17,12 +16,11 @@ import org.springframework.context.annotation.Configuration
 class UserRolesGrantFunctionImpl {
 
 	@Bean
-	fun userRolesGrantFunction(): UserRolesGrantFunction = f2Function { cmd ->
-		val realmClient = AuthRealmClientBuilder().build(cmd.auth)
+	fun userRolesGrantFunction(): UserRolesGrantFunction = keycloakF2Function { cmd, client ->
 		if (cmd.clientId == null) {
-			realmClient.addUserRealmRole(cmd.realmId, cmd.id, cmd.roles)
+			client.addUserRealmRole(cmd.realmId, cmd.id, cmd.roles)
 		} else {
-			realmClient.addUserClientRoles(cmd)
+			client.addUserClientRoles(cmd)
 		}
 		UserRolesGrantedEvent(cmd.id)
 	}
