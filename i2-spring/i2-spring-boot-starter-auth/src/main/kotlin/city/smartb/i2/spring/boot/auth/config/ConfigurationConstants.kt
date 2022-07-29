0af774115bdddcab768ad6e443aa118f7c1614be
Luc Heydel <luc@smartb.city>
2022-07-29 17:32:46 +0200

    fix web security config

--- i2-spring/i2-spring-boot-starter-auth/src/main/kotlin/city/smartb/i2/spring/boot/auth/config/ConfigurationConstants.kt
+++ i2-spring/i2-spring-boot-starter-auth/src/main/kotlin/city/smartb/i2/spring/boot/auth/config/ConfigurationConstants.kt
@@ -2,10 +2,10 @@ package city.smartb.i2.spring.boot.auth.config
 
 // properties
 const val I2_PREFIX = "i2"
-const val JWT_ISSUER_URIS = "$I2_PREFIX.issuers[0].uri"
+const val JWT_ISSUER_NAME = "$I2_PREFIX.issuers[0].name"
 
 // conditional expressions
-const val OPENID_REQUIRED_EXPRESSION = "!'\${$JWT_ISSUER_URIS:}'.isEmpty()"
+const val OPENID_REQUIRED_EXPRESSION = "!'\${$JWT_ISSUER_NAME:}'.isEmpty()"
 
 const val AUTHENTICATION_REQUIRED_EXPRESSION = "($OPENID_REQUIRED_EXPRESSION)"
 const val NO_AUTHENTICATION_REQUIRED_EXPRESSION = "!($AUTHENTICATION_REQUIRED_EXPRESSION)"
