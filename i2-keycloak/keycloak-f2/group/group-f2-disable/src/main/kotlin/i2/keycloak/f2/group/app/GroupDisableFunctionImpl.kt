e9524c586722628844516901f73a31b31f6bee0d
Luc Heydel <luc@smartb.city>
2022-08-10 12:54:54 +0200

    do not handle disabling of users when disabling a group (handled in im)

--- i2-keycloak/keycloak-f2/group/group-f2-disable/src/main/kotlin/i2/keycloak/f2/group/app/GroupDisableFunctionImpl.kt
+++ i2-keycloak/keycloak-f2/group/group-f2-disable/src/main/kotlin/i2/keycloak/f2/group/app/GroupDisableFunctionImpl.kt
@@ -1,6 +1,5 @@
 package i2.keycloak.f2.group.app
 
-import f2.dsl.cqrs.page.PagePagination
 import f2.dsl.fnc.f2Function
 import f2.dsl.fnc.invokeWith
 import i2.keycloak.f2.group.domain.features.command.GroupDisableFunction
@@ -8,11 +7,8 @@ import i2.keycloak.f2.group.domain.features.command.GroupDisabledEvent
 import i2.keycloak.f2.group.domain.features.command.GroupSetAttributesCommand
 import i2.keycloak.f2.group.domain.features.command.GroupSetAttributesFunction
 import i2.keycloak.f2.group.domain.model.GroupModel
-import i2.keycloak.f2.user.domain.features.command.UserDisableCommand
 import i2.keycloak.f2.user.domain.features.command.UserDisableFunction
 import i2.keycloak.f2.user.domain.features.query.UserPageFunction
-import i2.keycloak.f2.user.domain.features.query.UserPageQuery
-import i2.keycloak.f2.user.domain.model.UserModel
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -32,32 +28,8 @@ class GroupDisableFunctionImpl(
 			auth = cmd.auth
 		).invokeWith(groupSetAttributesFunction)
 
-		val userIds = UserPageQuery(
-			groupId = cmd.id,
-			withDisabled = false,
-			page = PagePagination(
-				page = 0,
-				size = Int.MAX_VALUE
-			),
-			realmId = cmd.realmId,
-			auth = cmd.auth
-		).invokeWith(userPageFunction)
-			.items
-			.items
-			.map(UserModel::id)
-
-		userIds.forEach { userId ->
-			UserDisableCommand(
-				id = userId,
-				realmId = cmd.realmId,
-				auth = cmd.auth
-			).invokeWith(userDisableFunction)
-		}
-
 		GroupDisabledEvent(
-			id = cmd.id,
-			userIds = userIds
+			id = cmd.id
 		)
 	}
-
 }
