5d64eb87e9d482d54e92806bfda2b1572e3a1b19
Adrien Poupard <adrien@smartb.city>
2023-01-28 22:13:10 +0100

    Expose i2-commons-api as api

--- i2-keycloak/keycloak-f2/group/i2-group-f2-query/build.gradle.kts
+++ i2-keycloak/keycloak-f2/group/i2-group-f2-query/build.gradle.kts
@@ -7,6 +7,6 @@ plugins {
 dependencies {
     api(project(":i2-keycloak:keycloak-f2:group:i2-group-domain"))
     implementation(project(":i2-keycloak:keycloak-f2:role:i2-role-f2-query"))
-    implementation(project(":i2-keycloak:keycloak-f2:commons:i2-commons-api"))
+    api(project(":i2-keycloak:keycloak-f2:commons:i2-commons-api"))
 
 }
