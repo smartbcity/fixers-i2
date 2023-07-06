1cc9d4496f2962bbcfe072138b4544e3d95cc375
Adrien Poupard <adrien@smartb.city>
2023-07-06 21:59:17 +0200

    refactor(build.gradle.kts): remove Spring Boot dependencies and tasks
    The Spring Boot dependencies and tasks were removed because the project is transitioning away from Spring Boot.

--- i2-app/core/build.gradle.kts
+++ i2-app/core/build.gradle.kts
@@ -1,15 +1,9 @@
 plugins {
-    id("org.springframework.boot")
     id("city.smartb.fixers.gradle.kotlin.jvm")
-    kotlin("plugin.spring")
 }
 
 dependencies {
     implementation(project(":i2-keycloak:keycloak-f2:config:i2-config-command"))
     Dependencies.Jvm.f2Http(::implementation)
     Dependencies.Jvm.slf4j(::implementation)
-}
-
-tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootBuildImage> {
-    imageName.set("smartbcity/i2-config:${this.project.version}")
 }
